12b42914f8039ca502b64e7a7e10749b61033480
Song <joon1769@gmail.com>
2020-06-09 17:12:05 +0900

    ddd

--- Alba_Go/src/main/java/com/spring/ex/controller/MyController.java
+++ Alba_Go/src/main/java/com/spring/ex/controller/MyController.java
@@ -18,7 +18,10 @@ public class MyController {
 	public String Mypage_Individual() {
 		return "/Mypage_Individual";
 	}
-	
+	@RequestMapping("/Talent/Human_resource_info")
+	public String Human_resource_info() {
+		return "/Talent/Human_resource_info";
+	}
 	@RequestMapping("/Mypage_enterprise")
 	public String Mypage_enterprise() {
 		return "/Mypage_enterprise";
@@ -30,6 +33,38 @@ public class MyController {
 	@RequestMapping("/Account_enterprise")
 	public String Account_enterprise() {
 		return "/Account_enterprise";
+	}
+	@RequestMapping("/Review_content")
+	public String Review_content() {
+		return "/Review_content";
+	}
+	@RequestMapping("/Review_list")
+	public String Review_list() {
+		return "/Review_list";
+	}
+	@RequestMapping("/Review_write")
+	public String Review_write() {
+		return "/Review_write";
+	}
+	@RequestMapping("/Manager_human_info")
+	public String Manager_human_info() {
+		return "/Manager_human_info";
+	}
+	@RequestMapping("/Manager_reviewlist")
+	public String Manager_reviewlist() {
+		return "/Manager_reviewlist";
+	}
+	@RequestMapping("/Manager_payment")
+	public String Manager_payment() {
+		return "/Manager_payment";
+	}
+	@RequestMapping("/Product_list")
+	public String Product_list() {
+		return "/Product_list";
+	}
+	@RequestMapping("/Payment")
+	public String Payment() {
+		return "/Payment";
 	}
 	
 }
