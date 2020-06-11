2cba2417a21a8190007a044d0c80b77c640f682b
sujeongGong <brightelf9@naver.com>
2020-06-11 15:15:29 +0900

    컨트롤러 나누기

--- Alba_Go/src/main/java/com/spring/ex/controller/MainController.java
+++ Alba_Go/src/main/java/com/spring/ex/controller/MainController.java
@@ -4,24 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class MyController {
-	@RequestMapping("/main")
+@RequestMapping("*.do")
+public class MainController {
+	@RequestMapping("/")
 	public String main() {
 		return "/main";
 	}
 	@RequestMapping("/test")
 	public String test() {
 		return "/test";
-	}
-	
-	@RequestMapping("/Mypage_Individual")
-	public String Mypage_Individual() {
-		return "/Mypage_Individual";
-	}
-	
-	@RequestMapping("/Account_Individual")
-	public String Account_Individual() {
-		return "/Account_Individual";
 	}
 	
 }
